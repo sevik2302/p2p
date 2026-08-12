eb19007cf636180aaa79157c4d3d129f98ff2cb1
sevik2302 <sevik2302@gmail.com>
2026-08-12 04:03:57 +0300

    Add files via upload

---
+++ src/routes/auth.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const ctrl = require('../controllers/authController');
+const { authRequired } = require('../middleware/auth');
+
+router.post('/register', ctrl.register);
+router.post('/login', ctrl.login);
+router.get('/me', authRequired, ctrl.me);
+
+module.exports = router;
